28772643a64f30f13c397c3321446b26a6825253
Marcos Oliveira <marcosoliveiiraa@gmail.com>
2022-03-21 16:51:47 -0300

    pratica08

---
+++ pratica08/scrip.js
@@ -0,0 +1,6 @@
+function modificaEstilo(corDeFundo, l, a){
+document.getElementById('quadrado').style.background = corDeFundo
+//para intergir com largura e altura
+document.getElementById('quadrado').style.width = l 
+document.getElementById('quadrado').style.height = a 
+}
