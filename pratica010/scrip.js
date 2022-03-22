27fb8f0633a80f595f7e49015ec30c736fc69286
Marcos Oliveira <marcosoliveiiraa@gmail.com>
2022-03-22 19:14:32 -0300

    pratica010

---
+++ pratica010/scrip.js
@@ -0,0 +1,11 @@
+var caracter = document.getElementById('input').value
+
+function acao(){
+ input = onmouseout.style.backgroundColor.yellow
+if(caracter >= 3){
+    Document.write == ''.length
+}if(caracter <= 3){
+    document.write == ''.length
+}
+    
+}
