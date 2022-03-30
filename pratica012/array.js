29abd8aeae00f87cb96ba3438f2b5bab7ddc77ae
marcos <marcosoliveiiraa@gmail.com>
2022-03-30 13:33:08 -0300

    pratica13

---
+++ pratica012/array.js
@@ -0,0 +1,15 @@
+var lista_dfrutas = Array()
+
+lista_dfrutas[1] = 'Banana'
+lista_dfrutas[2] = true
+lista_dfrutas['x'] = 'Morango'
+lista_dfrutas[4] = 100
+
+console.log(lista_dfrutas[2])
+
+/*outra forma de trabalhar com array:
+
+var lista_dfrutas = ['Banana','Maçã','Morango', 'Uva']
+console.log(lista_dfrutas)
+
+*/
